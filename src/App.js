518a882234653b3191bee50614c5a7e98fb5868d
James <james.ekasiba@student.moringaschool.com>
2023-12-12 10:37:33 +0300

    Add Review Form

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import { Routes, Route } from "react-router-dom";
-import { Container } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
 import Home from "./pages/Home";
 import AddReview from "./pages/AddReview";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <>
-    <Navbar />
-    <Container>
-<Routes>
-  <Route path="/" element={<Home />}></Route>
-  <Route path="add-review" element={<AddReview />}></Route>
-</Routes>
-</Container>
-    </>
+    <main style={{ height: "100vh" }}>
+      <Navbar />
+      <Box bg={"gray.200"} style={{ height: "100vh" }}>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="add-review" element={<AddReview />}></Route>
+        </Routes>
+      </Box>
+    </main>
   );
 }
 
--- src/pages/AddReview.jsx
+++ src/pages/AddReview.jsx
@@ -1,9 +1,42 @@
+import { Box, Flex, FormControl, FormLabel, Heading, Input, Stack, Textarea, Button } from '@chakra-ui/react'
 import React from 'react'
 
 function AddReview() {
   return (
-    <div>AddReview</div>
-  )
+    <Flex align={"center"} justify={"center"}>
+      <Stack>
+        <Heading fontSize={"4xl"}>Add Review</Heading>
+        <Box rotate={"lg"} bg={"white"} p={8}>
+          <Stack>
+            <FormControl isRequired>
+              <FormLabel>Name</FormLabel>
+              <Input name="name" placeholder="Name" />
+            </FormControl>
+
+            <FormControl isRequired>
+              <FormLabel>Review</FormLabel>
+              <Textarea name="name" placeholder="Movie review" />
+            </FormControl>
+
+            <FormControl isRequired>
+              <FormLabel>Date Posted</FormLabel>
+              <Input name="date_posted" type="datetime-local" />
+            </FormControl>
+
+            <FormControl isRequired>
+              <FormLabel>Ratings</FormLabel>
+              <Button colorScheme="blue">-</Button>
+              <span style={{ margin: "0 10px" }}></span>
+              <Button colorScheme="blue">+</Button>
+            </FormControl>
+            <Stack pt={3}>
+              <Button>Submit</Button>
+            </Stack>
+          </Stack>
+        </Box>
+      </Stack>
+    </Flex>
+  );
 }
 
 export default AddReview
